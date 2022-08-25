5d0585e6faa85ca542696569015a3cdb4382b082
Artem Bushmin <nim-shub@rambler.ru>
2022-08-25 23:10:09 +0300

    Локальная настройка изменеия адреса11

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 module.exports.auth = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
@@ -11,7 +13,7 @@ module.exports.auth = (req, res, next) => {
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
   } catch (error) {
     const err = new Error('Необходима авторизация');
     err.statusCode = 401;
