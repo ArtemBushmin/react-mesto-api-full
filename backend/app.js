ca4da4a736e4ba6b18dfc0e45e9ae86c40bfec69
Artem Bushmin <nim-shub@rambler.ru>
2022-08-25 22:28:36 +0300

    Локальная настройка изменеия адреса6

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -16,6 +17,11 @@ const app = express();
 app.use(express.json());
 app.use(requestLogger);
 app.use(cors());
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.post(
   '/signin',
   celebrate({
